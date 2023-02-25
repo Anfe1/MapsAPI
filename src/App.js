ca385ce136fbea8cd199592f7111d11fc6cf5d78
Andy Fernandez <andy.w.fernandez@gmail.com>
2023-02-25 14:14:24 -0700

    adde the tile data to display map.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import SearchBar from "./SearchBar";
+import Maps from "./Maps";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div
+      className="App"
+      style={{
+        border: "2px solid red",
+        display: "flex",
+        flexDirection: "row",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div style={{ border: "2px solid red", width: "50vw", height: "100%" }}>
+        <Maps />
+      </div>
+      <div style={{ border: "2px solid red", width: "50vw" }}>
+        <SearchBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Maps.js
+++ src/Maps.js
@@ -1,5 +1,24 @@
-import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+const position = [39.7392, -104.9903];
 
 export default function Maps() {
-  return <div>Maps</div>;
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=AVtr188MJ0IVEgVTcguy"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
